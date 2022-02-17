a7fb9d75f6da9ba379d862b18ae11791afc29bba
Maya Kano <mayakanoweb@gmail.com>
2022-02-16 21:59:06 -0500

    filter with dummy data

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Button({button, filter}) {
+  return (
+    <div>
+      {
+        button.map((cat, i)=> {
+          return <button type="button" onClick={()=> filter(cat)}>{cat}</button>
+        })
+      }
+    </div>
+  )
+}
+
+export default Button; 
---
+++ src/filtertag.js
@@ -0,0 +1,44 @@
+import python from './img/python.png';
+import javascript from './img/javascript.png';
+import sql from './img/sql.png';
+import react from './img/react.png';
+import java from './img/java.png';
+
+//dummy data in the form of objects in the array
+export default [
+    {
+        id: 1,
+        image: python,
+        title: 'Python expert', 
+        category: 'Python',
+        description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    },
+    {
+        id: 2,
+        image: javascript,
+        title: 'Javascript expert', 
+        category: 'Javascript',
+        description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    },
+    {
+        id: 3,
+        image: sql,
+        title: 'SQL expert', 
+        category: 'SQL',
+        description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    },
+    {
+        id: 4,
+        image: java,
+        title: 'Java expert', 
+        category: 'Java',
+        description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    },
+    {
+        id: 5,
+        image: react,
+        title: 'React expert', 
+        category: 'React',
+        description: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    }
+]
