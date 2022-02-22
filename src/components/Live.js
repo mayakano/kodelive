a79a439e78ab0314b66c5fcf2e0b103e6222a77e
Maya Kano <mayakanoweb@gmail.com>
2022-02-22 14:15:48 -0500

    changes to conditions

--- src/components/Live.js
+++ src/components/Live.js
@@ -17,43 +17,44 @@
 // };
 
 // export default Home;
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from "react";
 // import { roomUrlFromPageUrl } from "../utils/urlUtils";
-import api from '../utils/api';
-import DailyIframe from '@daily-co/daily-js';
+import api from "../utils/api";
+import DailyIframe from "@daily-co/daily-js";
 
-const STATE_IDLE = 'STATE_IDLE';
-const STATE_JOINING = 'STATE_JOINING';
-const STATE_JOINED = 'STATE_JOINED';
+const STATE_IDLE = "STATE_IDLE";
+const STATE_JOINING = "STATE_JOINING";
+const STATE_JOINED = "STATE_JOINED";
 
-const Live = ({list}) => {
-    
+//tak your time:)
+
+const Live = ({ list }) => {
   const [appState, setAppState] = useState(STATE_IDLE);
   const [roomUrl, setRoomUrl] = useState(null);
   const [callObject, setCallObject] = useState(null);
-  const [clickable, setClickable] = useState(false);
+  const [selected, setSelected] = useState(false);
 
-  const optionSelected = (list) => {
- 
-  }
-  
   const createCall = useCallback(() => {
     return api
       .createRoom()
       .then((room) => {
-            console.log("api.createRoom room: ", room);
-            window.open(room.url, '_blank')
-            return room.url
-        })
+        console.log("api.createRoom room: ", room);
+        window.open(room.url, "_blank");
+        return room.url;
+      })
       .catch((error) => {
-        console.log('Error creating room', error);
+        console.log("Error creating room", error);
         setRoomUrl(null);
         setAppState(STATE_IDLE);
       });
   }, []);
 
+  const setEnableGoLive = () => {
+    setSelected(true);
+  };
+
   const startJoiningCall = useCallback((url) => {
-      console.log("startJoiningCall url", url);
+    console.log("startJoiningCall url", url);
     const newCallObject = DailyIframe.createCallObject();
     setRoomUrl(url);
     setCallObject(newCallObject);
@@ -61,26 +62,26 @@ const Live = ({list}) => {
     newCallObject.join({ url });
   }, []);
 
-  
-//   useEffect(() => {
-//     const url = roomUrlFromPageUrl();
-//     url && startJoiningCall(url);
-//   }, [startJoiningCall]);
+  //   useEffect(() => {
+  //     const url = roomUrlFromPageUrl();
+  //     url && startJoiningCall(url);
+  //   }, [startJoiningCall]);
 
-//   useEffect(() => {
-//     const pageUrl = pageUrlFromRoomUrl(roomUrl);
-//     if (pageUrl === window.location.href) return;
-//     window.history.replaceState(null, null, pageUrl);
-//   }, [roomUrl]);
+  //   useEffect(() => {
+  //     const pageUrl = pageUrlFromRoomUrl(roomUrl);
+  //     if (pageUrl === window.location.href) return;
+  //     window.history.replaceState(null, null, pageUrl);
+  //   }, [roomUrl]);
 
   useEffect(() => {
     if (!callObject) return;
 
-    const events = ['joined-meeting', 'left-meeting', 'error'];
+    const events = ["joined-meeting", "left-meeting", "error"];
 
     function handleNewMeetingState(event) {
       event && console.log("Daily logEvent", event);
-      if (callObject.meetingState() == 'joined-meeting') setAppState(STATE_JOINED);
+      if (callObject.meetingState() == "joined-meeting")
+        setAppState(STATE_JOINED);
     }
 
     // Use initial state
@@ -90,19 +91,23 @@ const Live = ({list}) => {
     for (const event of events) {
       callObject.on(event, handleNewMeetingState);
     }
-
   }, [callObject]);
 
+  //this is where I'm rendering the selections that you see at the bottom Okay
 
   return (
-    
-<div className='text-center'>
-    <h3 className='font-medium leading-tight text-3xl mt-10 mb-2 text-white-700'>Looking to help out? <br />
-    <span className='text-2xl'>Select your skills</span></h3>
-      { 
-        list.map((category, i)=> {
-          return (
-              <button className='
+    <div className="text-center">
+      <h3 className="font-medium leading-tight text-3xl mt-10 mb-2 text-white-700">
+        Looking to help out? <br />
+        <span className="text-2xl">Select your skills</span>
+      </h3>
+      {console.log(list)}
+      {/* {list.map((category, i) => { */}
+      {/* return (
+      <button
+        // key={}
+        onClick={() => setEnableGoLive(true)}
+        className="
               place-self-center	
               place-items-center
               place-content-center	
@@ -112,25 +117,28 @@ const Live = ({list}) => {
               text-white 
               font-bold 
               py-2 px-4 
-              rounded-full' 
-              type="button">{category}</button>
-        )})
-      }
-    <span className='container'>
-    {/* {optionSelected == true && */}
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded px-16 mt-6"
-            onClick={() => {
-              createCall().then((url) => startJoiningCall(url));
-            }}
-          >
-            Go Live
-          </button>
-  {/* } */}
-  </span>
-</div>
+              rounded-full"
+        type="button"
+      > */}
+      {list}
+      {list.map((category, i) => {
+        <button key={i}>{category}</button>;
+      })}
+      <span className="container">
+        {/* {optionSelected == true && */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded px-16 mt-6"
+          disabled={!selected}
+          onClick={() => {
+            createCall().then((url) => startJoiningCall(url));
+          }}
+        >
+          Go Live
+        </button>
+        {/* } */}
+      </span>
+    </div>
   );
 };
 
 export default Live;
-
